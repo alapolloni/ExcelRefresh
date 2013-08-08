2b3a0492ad41a4ea36b3ec2c3b91d054d6a3616e
Alex Apolloni <apolloni@yahoo.com>
2013-08-08 15:55:21 +0200

    Pretty much working. Added command line options via add-in Added README.md Come clean up

--- Program.cs
+++ Program.cs
@@ -6,22 +6,23 @@ using Excel=Microsoft.Office.Interop.Excel;
 using Microsoft.Office.Tools.Excel;
 using CommandLine;
 using CommandLine.Text;
+using System.IO;
 
 namespace XLRefresh
 {
     class Program
     {
-        static void useQueryTables(Excel.Workbook theWorkbook)
+        private static void refreshQueryTables(Excel.Workbook theWorkbook)
         {
-            Console.WriteLine("wb"); 
+            Console.WriteLine("WorkSheets:"); 
             Excel.Sheets oSheets = (Excel.Sheets)theWorkbook.Worksheets;
          
             foreach (Excel.Worksheet oWorkSheet in oSheets)
             {
-               Console.WriteLine(oWorkSheet.Name);
+               Console.WriteLine(" {0}",oWorkSheet.Name);
                foreach (Excel.QueryTable qt in oWorkSheet.QueryTables)
                 {                   
-                    Console.WriteLine(" qt");
+                    Console.WriteLine("--qt:{0}",qt.Name);
                     qt.EnableRefresh = true;
                     qt.FieldNames = false;
                     qt.RowNumbers = false;
@@ -34,9 +35,46 @@ namespace XLRefresh
                       
             return;            
         }
+        
+        private static void refreshPivots(Excel.Workbook theWorkbook)
+        {
 
+            Console.WriteLine("WorkSheets:"); 
+            Excel.Sheets oSheets = (Excel.Sheets)theWorkbook.Worksheets;
 
+            foreach (Excel.Worksheet oWorkSheet in oSheets)
+            {
+                Console.WriteLine(" {0}",oWorkSheet.Name);
+                Excel.PivotTables pivotTables1 =
+                    (Excel.PivotTables)oWorkSheet.PivotTables();
 
+                if (pivotTables1.Count > 0)
+                {
+                    for (int i = 1; i <= pivotTables1.Count; i++)
+                    {
+                        Console.WriteLine("  PivoteTable Refresh: {0}", pivotTables1.Item(i).Name);
+                        pivotTables1.Item(i).RefreshTable();
+                        
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("  !This worksheet contains no pivot tables.");
+                }
+
+            }
+
+        }
+
+        private static void refreshConnection(Excel.Workbook theWorkbook)
+        {
+            foreach (Microsoft.Office.Interop.Excel.WorkbookConnection i in theWorkbook.Connections)
+            {
+                Console.WriteLine("Connection refresh: {0}",i.Name);
+                i.OLEDBConnection.BackgroundQuery = false;
+                i.Refresh();
+            }
+        }
 
 
         static void Main(string[] args)
@@ -45,18 +83,30 @@ namespace XLRefresh
             var options = new Options();
             if (CommandLine.Parser.Default.ParseArguments(args, options))
             {
-                Console.WriteLine("options, in the IF");
                 // Values are available here
                 if (options.Verbose) Console.WriteLine("Filename: {0}", options.InputFile);
+                //Items?
+                if (options.Verbose)
+                {
+                    Console.WriteLine("Items Count: {0}", options.Items.Count.ToString());
+                    options.Items.ToList().ForEach(i => Console.Write("{0}\t", i));
+                }
             }
-            else {
-                Console.WriteLine("working ...");
-                Console.WriteLine("Press any key to close...");
-                Console.ReadLine();
+            else
+            {
+                Console.WriteLine("required options not specified ... quiting.");
                 return;
             }
 
-            string txtLocation = "C:/Users/aapollon/Documents/TPT/test xlrefresh/Sherpa_test - Copy.xlsx";
+
+            string txtLocation = Path.GetFullPath(options.InputFile);
+            if (options.Verbose) Console.WriteLine("Input File Full Path: {0}", txtLocation);
+            if (! File.Exists(txtLocation))
+            {
+                Console.WriteLine("Input File does not exist: {0}", txtLocation);
+                return;
+            }
+           
             object _missingValue = System.Reflection.Missing.Value;
             Microsoft.Office.Interop.Excel.Application excel = new Microsoft.Office.Interop.Excel.Application();
             Excel.Workbook theWorkbook = excel.Workbooks.Open(txtLocation,
@@ -72,70 +122,58 @@ namespace XLRefresh
                                                             _missingValue,
                                                             _missingValue,
                                                             _missingValue);
-            excel.Visible=true;
-            //doesn't work//useQueryTables(theWorkbook);
             
-            //useConnection(theWorkbook);
-            refreshPivots(theWorkbook);
+            if (options.Visable) excel.Visible = true;
+            if (options.Querytables) { refreshQueryTables(theWorkbook); }
+            if (options.Connections) { refreshConnection(theWorkbook); }
+            if (options.Pivottables) { refreshPivots(theWorkbook); }
+
+            Console.WriteLine("shut it down!");
+            excel.Calculate();
+            Console.WriteLine("calculated");
+            
+            
+
             theWorkbook.Save();
-            theWorkbook.Close(); 
+            Console.WriteLine("saved");
+            theWorkbook.Close(true);
+            Console.WriteLine("closed");
             excel.Quit();
-            Console.WriteLine("Press any key to close...");
-            Console.ReadLine();
+            Console.WriteLine("Quit");
+            //Console.WriteLine("Press any key to close...");
+            //Console.ReadLine();
             return;
-                
         }
-
-        private static void refreshPivots(Excel.Workbook theWorkbook)
-        {
-            
-            Console.WriteLine("wb"); 
-            Excel.Sheets oSheets = (Excel.Sheets)theWorkbook.Worksheets;
-
-            foreach (Excel.Worksheet oWorkSheet in oSheets)
-            {
-                Console.WriteLine(oWorkSheet.Name);
-                Excel.PivotTables pivotTables1 =
-                    (Excel.PivotTables)oWorkSheet.PivotTables();
-
-                if (pivotTables1.Count > 0)
-                {
-                    for (int i = 1; i <= pivotTables1.Count; i++)
-                    {
-                        Console.WriteLine("PT update");
-                        pivotTables1.Item(i).RefreshTable();
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("This workbook contains no pivot tables.");
-                }
-
-            }
-
-        }
-
-        private static void useConnection(Excel.Workbook theWorkbook)
-        {
-            foreach (Microsoft.Office.Interop.Excel.WorkbookConnection i in theWorkbook.Connections)
-            {
-                System.Console.WriteLine(i.Name);
-                i.OLEDBConnection.BackgroundQuery = false;
-                i.Refresh();
-            }
-        }
-
     }
 
 // Define a class to receive parsed values
 class Options {
-  [Option('r', "read", Required = true,
+  [Option('f', "file", Required = true,
     HelpText = "Input file to be processed.")]
   public string InputFile { get; set; }
-    
-  [Option('v', "verbose", DefaultValue = true,
-    HelpText = "Prints all messages to standard output.")]
+
+  [Option('d', "verbose", DefaultValue = false,
+      HelpText = "Prints all messages to standard output.")]
   public bool Verbose { get; set; }
+
+  [Option('v', "visable", DefaultValue = false,
+    HelpText = "Shows Excel.")]
+  public bool Visable { get; set; }
+
+  [Option('p', "pivot-tables", DefaultValue = false,
+      HelpText = "Refresh Pivot-tables.")]
+  public bool Pivottables { get; set; }
+
+  [Option('q', "query-tables", DefaultValue = false,
+  HelpText = "Refresh query-tables.")]
+  public bool Querytables { get; set; }
+
+  [Option('c', "connections", DefaultValue = false,
+        HelpText = "Refresh External connections.")]
+  public bool Connections { get; set; }
+    
+  [ValueList(typeof(List<string>), MaximumElements = 6)]
+    public IList<string> Items { get; set; }
 
   [ParserState]
   public IParserState LastParserState { get; set; }
@@ -146,6 +184,9 @@ class Options {
       (HelpText current) => HelpText.DefaultParsingErrorsHandler(this, current));
   }
 }
+
+
+
 
 
 
---
+++ README.md
@@ -0,0 +1,12 @@
+XLRefreshC.exe (XLRefresh in C#)
+
+asdf
+
+ - The "DO you want to Save keeps popping up intermitently".  Test by adding sleeps...i just guessing.
+  - http://stackoverflow.com/questions/2123158/c-sharp-winforms-how-to-load-an-image-then-wait-a-few-seconds-then-play-a-mp3
+  - which suggests application.doevents()
+  - er...maybe adding a save:true to the close was good enough....
+ - ilmerge get working (now need to distribute command.dll with .exe)
+ - upload to github.com
+ - Add do --all option.
+ - Add run --macros option
